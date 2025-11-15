9c58eec2653f1e11af9f17e2fbf2a90a7660d2ca
Tarek Yehia Gaber Mahmoud Khaled <Tarek.Gaber@te.eg>
2025-11-15 21:55:07 +0200

    update build with correct base

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important: This makes all asset paths relative.
-  build: {
-    outDir: 'dist',
-  },
-  // Tauri configuration
-  clearScreen: false,
-  server: {
-    port: 1420,
-    strictPort: true,
-  },
-  envPrefix: ['VITE_', 'TAURI_'],
+  base: '/GarhyPortal/',   // ← مهم جداً
 })
