6b425534c63968bf84957e840653bf9218c330b2
Tarek Yehia Gaber Mahmoud Khaled <Tarek.Gaber@te.eg>
2025-11-15 22:01:44 +0200

    Add GitHub Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GarhyPortal/',   // ← مهم جداً
+  base: "/GarhyPortal/",
 })
